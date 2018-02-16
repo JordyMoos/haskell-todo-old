51fcce9166ed4c1ed584436f61f6b47057002016
Jordy Moos <jordymoos@gmail.com>
2018-02-16 14:18:57 +0100

    work on the command line parser

---
+++ README.md
@@ -0,0 +1,6 @@
+# Todo list application
+
+
+### Development
+
+`stack build --fast --file-watch --exec todo-exe`
--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,22 @@ import Options.Applicative
 
 type ItemIndex = Int
 
-
 defaultDataPath :: FilePath
 defaultDataPath = "./.todo.yaml"
 
 dataPathParser :: Parser FilePath
 dataPathParser = strOption $
-    value defaultDataPath
-    <> long "data-path"
-    <> short 'p'
-    <> metavar "DATAPATH"
-    <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+  value defaultDataPath
+  <> long "data-path"
+  <> short 'p'
+  <> metavar "DATAPATH"
+  <> help ("path to data file (default " ++ defaultDataPath ++ ")")
 
 itemIndexParser :: Parser ItemIndex
 itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
 
-
 main :: IO ()
 main = do
-    dataPath <- execParser (info (dataPathParser) (progDesc "Todo list manager"))
-    putStrLn $ "dataPath=" ++ show dataPath
---     itemIndex <- execParser (info (itemIndexParser) (progDesc "Todo list manager"))
---     putStrLn $ "itemIndex=" ++ show itemIndex
+  -- itemIndex <- execParser (info itemIndexParser (progDesc "Todo list manager"))
+  dataPath <- execParser (info dataPathParser (progDesc "Todo list manager"))
+  putStrLn $ "dataPath=" ++ show dataPath
